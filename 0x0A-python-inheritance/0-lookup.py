37a73c91d690d1b8f9e1b2dc16eeab99b240a76b
MaryBaptista <marybaptistanwachukwu@gmail.com>
2022-08-02 03:12:06 +0000

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+ttribute lookup module
+"""
+
+
+def lookup(obj):
+    """ Look up function
+        Args: object to look of attributes
+        Return: list of attributes of that object
+    """
+    if isinstance(obj, object):
+        return list(dir(obj))
